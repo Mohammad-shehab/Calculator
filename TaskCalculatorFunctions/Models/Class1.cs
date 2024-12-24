fbaafbc827660370991f675fc8d92cce07f9f848
Mohammad-shehab <q8mlg@outlook.com>
2024-12-24 15:22:46 +0300

    Add project files.

---
+++ TaskCalculatorFunctions/Models/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskCalculatorFunctions.Models
+{
+    internal class Class1
+    {
+
+        public double add(double num1, double num2)
+        {
+            return num1 + num2; 
+        }
+        public double add(double num1, double num2,double num3)
+        {
+            return   num1 + num2 + num3;
+        }
+
+        public int add(int num1, int num2, int num3)
+        {
+            return num1 + num2 + num3;
+        }
+
+
+        public string salary(double salary, int work_hour)
+        {
+
+
+
+
+            return $"Total salary: {salary+(work_hour*10)}";
+        }
+
+
+
+
+
+
+    }
+}
---
+++ TaskCalculatorFunctions/Program.cs
@@ -0,0 +1,59 @@
+﻿//1. Create 3 functions  name : Add
+//2.First one 2 Parameter and sum 
+//3. Calc summation of 3 parameters
+//4. 3 parameters , Calc Summation
+//5. Print Salary with bounce (Work hour * hour rate=10 KD)
+//6. Call function by main program and print all result
+
+
+using System;
+using TaskCalculatorFunctions.Models;
+
+Console.WriteLine("Function 1");
+Console.WriteLine("Enter 2 numbers: ");
+double num1 = Convert.ToDouble(Console.ReadLine());
+double num2 = Convert.ToDouble(Console.ReadLine());
+Class1 sum = new Class1();
+
+Console.WriteLine($"Sum :  {sum.add(num1, num2)}");
+
+
+////////////////////////////////////////////////////////////
+
+
+Console.WriteLine("------------------------------------------");
+
+Console.WriteLine("Function 2");
+Console.WriteLine("Enter 3 numbers: ");
+double num3 = Convert.ToDouble(Console.ReadLine());
+double num4 = Convert.ToDouble(Console.ReadLine());
+double num5 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"Sum :  {sum.add(num3, num4, num5)}");
+
+
+////////////////////////////////////////////////////////////
+Console.WriteLine("------------------------------------------");
+
+
+Console.WriteLine("Function 3 (INTEGERS ONLY)");
+Console.WriteLine("Enter 3 numbers: ");
+int num6 = Convert.ToInt32(Console.ReadLine());
+int num7 = Convert.ToInt32(Console.ReadLine());
+int num8 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Sum :  {sum.add(num6, num7, num8)}");
+
+////////////////////////////////////////////////////////////
+Console.WriteLine("------------------------------------------");
+
+
+Console.WriteLine("Function 4");
+Console.WriteLine("Enter your salary: ");
+double salary = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter your work hours: ");
+int work_hour = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(sum.salary(salary, work_hour));
+
+
+
+
+
